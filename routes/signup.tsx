82edb951b262f9ea356eff6876770db83a9b8d90
suzukisan22 <getty2017@gmail.com>
2022-12-25 11:18:31 +0900

    新規登録時にログイン状態の設定

--- routes/signup.tsx
+++ routes/signup.tsx
@@ -2,7 +2,10 @@ import { Head } from "$fresh/runtime.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { PrismaClient } from '../generated/client/deno/edge.ts';
 import { Prisma } from '../generated/client/index.d.ts';
+import { setCookie } from "https://deno.land/std@0.167.0/http/cookie.ts";
+import { create, decode } from "https://deno.land/x/djwt@v2.8/mod.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt@v0.3.0/mod.ts";
+
 
 const prisma = new PrismaClient();
 
@@ -10,6 +13,7 @@ type UserInput = {
   email: string;
   name: string;
   password: string;
+  message: string;
 }
 
 export const handler: Handlers<UserInput | null> = {
@@ -17,28 +21,34 @@ export const handler: Handlers<UserInput | null> = {
     return ctx.render({
       email: '',
       name: '',
-      password: ''
+      password: '',
+      message: ''
     });
   },
   async POST(req: Request, ctx) {
     const data = await req.formData()
-    console.log('data')
-    console.log(data)
-    // const data1 = await fetch('http://localhost:8000/api/test', {method: 'POST', body: JSON.stringify({
-    //   email: data.get('email'),
-    //   password: data.get('password')
-    // })})
-    // console.log(data1)
     const email = data.get('email') as string
     const password = data.get('password') as string
+
     const name = data.get('name') as string
     if (email == null || password == null) {
       return ctx.render({
         email: email,
         name: name,
-        password: password
-      });
-    };
+        password: password,
+        message: ''
+       });
+    }
+
+    const emailDuplicate = await prisma.user.findFirst({where: {email}})
+    if (emailDuplicate != null) {
+      return ctx.render({
+        email: email,
+        name: name,
+        password: password,
+        message: '重複しています'
+       });
+    }
 
     const user = {
       email,
@@ -46,14 +56,35 @@ export const handler: Handlers<UserInput | null> = {
       name: data.get('name')
     } as Prisma.UserCreateInput
 
-    await prisma.user.create({
+    const userResponse = await prisma.user.create({
       data: user
     })
-    return ctx.render({
+
+    const key = await crypto.subtle.generateKey(
+      { name: "HMAC", hash: "SHA-512" },
+      true,
+      ["sign", "verify"],
+    );
+
+    const dt = new Date
+    dt.setDate(dt.getDate() + 10);
+
+    const jwt = await create({ alg: "HS512", typ: "JWT" }, { user_id: userResponse.id, exp: Number(dt) }, key);
+    const response = await ctx.render({
       email: '',
       name: '',
-      password: ''
+      password: '',
+      message: ''
     });
+
+    setCookie(response.headers, {
+      name: "passkey-example-session",
+      value: jwt,
+      maxAge: 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
+
+    return response;
   }
 };
 
@@ -65,6 +96,7 @@ export default function Signup({ data }: PageProps<UserInput>) {
         <title>Fresh App</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
+        {data.message && <p>{data.message}</p>}
         <form method='post'>
           <div>
             <label>
@@ -75,7 +107,7 @@ export default function Signup({ data }: PageProps<UserInput>) {
           <div>
             <label>
               name: 
-              <input type='text' name='name' required class={`px-3 py-2 bg-white rounded border(gray-500 2) disabled:(opacity-50 cursor-not-allowed)`} value={data.email} />
+              <input type='text' name='name' required class={`px-3 py-2 bg-white rounded border(gray-500 2) disabled:(opacity-50 cursor-not-allowed)`} value={data.name} />
             </label>
           </div>
           <div>
