09e3f6cc5cee5efe3c662b557bbe6bf3f4062c50
suzukisan22 <getty2017@gmail.com>
2023-01-15 11:01:17 +0900

    各ページのログイン制御

--- routes/account.tsx
+++ routes/account.tsx
@@ -1,15 +1,53 @@
 import { Head } from "$fresh/runtime.ts";
 import Passkey from "../islands/Passkey.tsx";
+import { PrismaClient } from '../generated/client/deno/edge.ts';
 import { HandlerContext, PageProps } from "$fresh/server.ts";
 import { getCookies } from "https://deno.land/std@0.167.0/http/cookie.ts";
+import { decode } from "https://deno.land/x/djwt@v2.8/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
+
+const prisma = new PrismaClient();
 
 type AccountProps = {
   cookie: string;
 }
 
+type JwtPayload = {
+  userId: number;
+  exp: number
+}
+
 export const handler = async (req: Request, ctx: HandlerContext): Promise<Response> => {
   const cookie = getCookies(req.headers)['passkey-example-session']
+  if (!cookie) {
+    return new Response("", {
+      status: 303,
+      headers: { Location: '/' },
+    });
+  }
+
+  const [headers, payload, signature] = decode(cookie)
+
+  const date = new Date();
+  const payloadJwt = payload as JwtPayload;
+  if (payloadJwt['exp'] < Number(date)) {
+    // jwtの期限が切れている場合
+    return new Response("", {
+      status: 303,
+      headers: { Location: '/' },
+    });
+  }
+
+  const userId = await prisma.user.findFirst({where: {id: payloadJwt.userId}});
+
+  if (!userId) {
+    // userIdがDBに存在しない場合
+    return new Response("", {
+      status: 303,
+      headers: { Location: '/' },
+    });
+  }
+
   return await ctx.render({cookie})
 };
 
--- routes/login.tsx
+++ routes/login.tsx
@@ -1,9 +1,43 @@
 import { Head } from "$fresh/runtime.ts";
 import LoginPasskeyForm from '../islands/LoginPasskeyForm.tsx';
-import { HandlerContext, PageProps } from "$fresh/server.ts";
+import { HandlerContext } from "$fresh/server.ts";
+import { getCookies } from "https://deno.land/std@0.167.0/http/cookie.ts";
+import { decode } from "https://deno.land/x/djwt@v2.8/mod.ts";
+import { PrismaClient } from '../generated/client/deno/edge.ts';
+
+const prisma = new PrismaClient();
+
+type JwtPayload = {
+  userId: number;
+  exp: number
+}
 
 export const handler = async (req: Request, ctx: HandlerContext): Promise<Response> => {
-  return await ctx.render({})
+  const cookie = getCookies(req.headers)['passkey-example-session']
+  if (!cookie) {
+    return ctx.render({})
+  }
+
+  const [headers, payload, signature] = decode(cookie)
+
+  const date = new Date();
+  const payloadJwt = payload as JwtPayload;
+  if (payloadJwt['exp'] < Number(date)) {
+    // jwtの期限が切れている場合
+    return ctx.render({})
+  }
+
+  const userId = await prisma.user.findFirst({where: {id: payloadJwt.userId}});
+
+  if (!userId) {
+    // userIdがDBに存在しない場合
+    return ctx.render({})
+  }
+
+  return new Response("", {
+    status: 303,
+    headers: { Location: '/' },
+  });
 };
 
 export default function Login() {
